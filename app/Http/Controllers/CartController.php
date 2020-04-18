e3f2d97d20636e356034f8d7d5d65d53486d2a77
ndquang1893 <ndquang1893@gmail.com>
2020-04-18 08:35:34 +0700

    Update giỏ hàng: lần 1

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -15,6 +15,7 @@ class CartController extends Controller
     public function save_cart(Request $request){
     	$productId = $request->productid_hidden;
     	$quantity = $request->qty;
+
     	$product_info = DB::table('tbl_product')->where('product_id',$productId)->first(); 
 
     
@@ -33,8 +34,8 @@ class CartController extends Controller
     }
     public function show_cart(){
 
-        $cate_product = DB::table('tbl_category_product')->where('category_status','0')->orderby('category_id','desc')->get(); 
-        $brand_product = DB::table('tbl_brand')->where('brand_status','0')->orderby('brand_id','desc')->get(); 
+        $cate_product = DB::table('tbl_category_product')->where('category_status','1')->orderby('category_id','desc')->get(); 
+        $brand_product = DB::table('tbl_brand')->where('brand_status','1')->orderby('brand_id','desc')->get(); 
         return view('pages.cart.show_cart')->with('category',$cate_product)->with('brand',$brand_product);
     }
     public function delete_to_cart($rowId){
